f02849ae1eb6d1929445559eab33ed1952c57837
UMARAJAZ11A <67511351+UMARAJAZ11A@users.noreply.github.com>
2022-11-19 06:44:47 +0530

    add

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/server
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
-yum -y install nodejs npm@6.12.0
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+yum -y install nodejs npm@8.3.0
